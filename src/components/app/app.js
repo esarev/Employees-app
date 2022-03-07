43bc463fabadc307ba85c64d6fa09a6e7cfaa8a5
Александр <esarev@inbox.ru>
2022-03-07 23:53:08 +0700

    React immutable

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,61 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {name: 'Andrey P.', salary: 800, increase: false, id: 1},
+        {name: 'Stepan B.', salary: 3000, increase: true, id: 2},
+        {name: 'Alex E.', salary: 5000, increase: false, id: 3},
+      ]
+    }
+    this.maxId = 4;
+  }
 
-  const data = [
-    {name: 'Andrey P.', salary: 800, increase: false, id: 1},
-    {name: 'Stepan B.', salary: 3000, increase: true, id: 2},
-    {name: 'Alex E.', salary: 5000, increase: false, id: 3},
-  ];
-
-  return (
-    <div className="app">
-      <AppInfo />
-
-      <div className="search-panel">
-        <SearchPanel/>
-        <AppFilter/>
-      </div>
+  deleteItem = id => {
+    this.setState(({data}) => {
       
-      <EmployersList data={data}/>
-      <EmployersAddForm/>
-    </div>
-  );
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    })
+  }
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      id: this.maxId++
+    }
+    this.setState(({data}) => {
+      const newArr = [...data, newItem];
+
+      return {
+        data: newArr
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+  
+        <div className="search-panel">
+          <SearchPanel/>
+          <AppFilter/>
+        </div>
+        
+        <EmployersList 
+          data={this.state.data}
+          onDelete={this.deleteItem}/>
+        <EmployersAddForm onAdd={this.addItem}/>
+      </div>
+    );
+  }
 }
 
 export default App;
